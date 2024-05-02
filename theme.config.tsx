bd9fd98561a5da0094fd77f6a573d5f3d62b9259
Andrew Young <andrew.young@ukr.net>
2024-05-02 11:52:42 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Pixada</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/andrewtheyoung/pixada',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/andrewtheyoung/pixada-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '(C) 2024 Andrew Young. All Rights Reserved.',
   },
 }
 
